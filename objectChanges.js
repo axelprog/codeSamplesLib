a13b96d38a061951780045d44073ef7079ba982c
Aleksey <lordaxel@gmail.com>
2020-12-07 15:24:13 +0300

    Improve object comparator

--- objectChanges.js
+++ objectChanges.js
@@ -6,24 +6,54 @@ function isNullBlankOrUndefined(o) {
   return (typeof o === 'undefined' || o == null || o === '');
 }
 
-export const objectChanges = function (object, base, ignoreBlanks = false) {
-  if (!isObject(object) || isDate(object)) return object;
+export const objectChanges = (object, base, ignoreBlanks = false, isUndefEqualFalse = false) {
+      if (!_.isObject(object) || _.isDate(object)) return object;
 
-  return transform(object, (result, value, key) => {
-    let comparableValue = value;
+      return _.transform(object, (result, value, key) => {
+        if (key === '$$hashKey') {
+          return;
+        }
 
-    if (value instanceof Set) {
-      comparableValue = Array.from(value);
-    }
+        //compare removed items  in arrays
+        if(_.isEmpty(result) && Array.isArray(result) &&  object.length < base.length) {
+          result.push(base.slice(object.length));
+        }
 
-    if (!isEqual(comparableValue, base[key])) {
-      if (ignoreBlanks && isNullBlankOrUndefined(comparableValue) && isNullBlankOrUndefined(base[key])) {
-        return;
-      }
-      // eslint-disable-next-line no-param-reassign
-      result[key] = isObject(comparableValue) && isObject(base[key])
-        ? objectChanges(comparableValue, base[key], ignoreBlanks)
-        : comparableValue;
-    }
-  });
-};
+        let comparableValue = value;
+
+        if (value instanceof Set) {
+          comparableValue = Array.from(value);
+        }
+
+        //compare for moment.js objects
+        if(moment.isMoment(comparableValue) || moment.isMoment(base[key])) {
+          if(!moment.utc(comparableValue).isSame(base[key])) {
+            result[key] = comparableValue;
+          }
+          return;
+        }
+
+        if (!_.isEqual(comparableValue, base[key])) {
+          if (ignoreBlanks && isNullBlankOrUndefined(comparableValue) && isNullBlankOrUndefined(base[key])) {
+            return;
+          }
+
+          if (isUndefEqualFalse
+            && ((isNullBlankOrUndefined(comparableValue) && base[key] === false)
+              || (comparableValue === false && isNullBlankOrUndefined(base[key])))) {
+            return;
+          }
+
+          if (_.isObject(comparableValue) && _.isObject(base[key])) {
+            const value = objectChanges(comparableValue, base[key], ignoreBlanks, isUndefEqualFalse);
+            if(value && !_.isEmpty(value)) {
+              result[key] = value;
+            }
+          } else {
+            result[key] = comparableValue;
+          }
+        }
+      }, null);
+    };
+
+
